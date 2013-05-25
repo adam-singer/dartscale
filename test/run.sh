f4156e7cd5421e6c7f165f4a63abd5c898339d8d
martyglaubitz <martyglaubitz@googlemail.com>
2013-05-25 02:31:49 +0200

    change: fix test script

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
-dart testMessageSubscription.dart
-dart testModuleLifecyle.dart
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dart $SCRIPTPATH/test/testMessageSubscription.dart
+dart $SCRIPTPATH/test/testModuleLifecyle.dart
 
