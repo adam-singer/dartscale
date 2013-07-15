b40587043d30bd61c93d767e70d73116e47b90c7
martyglaubitz <martyglaubitz@googlemail.com>
2013-07-16 00:53:08 +0200

    change: composit of StreamController for Mediator

--- lib/src/Mediator.dart
+++ lib/src/Mediator.dart
@@ -1,17 +1,33 @@
 part of dartscale;
 
+
+
+class MediatorTopic {
+  
+  StreamController _streamController = new StreamController.broadcast(sync: true);
+  
+  StreamSubscription listen(void onData(dynamic event), 
+                            {void onError(error), void onDone(), bool cancelOnError}){
+    return this._streamController.stream.listen(onData, onError: onError, onDone: onDone, cancelOnError: cancelOnError);
+  }
+  
+  void add(dynamic event) {
+    this._streamController.add(event);
+  }
+}
+
 class MediatorChannel {
   
-  var _topics = new Map<String, StreamController>();
+  var _topics = new Map<String, MediatorTopic>();
   
-  StreamController operator [](String topicName) {
-    StreamController stream = _topics[topicName];
-    if (stream == null) {
-      stream = new StreamController();
-      _topics[topicName] = stream;
+  MediatorTopic operator [](String topicName) {
+    MediatorTopic topic = _topics[topicName];
+    if (topic == null) {
+      topic = new MediatorTopic();
+      _topics[topicName] = topic;
     }
     
-    return stream;
+    return topic;
   }
 }
 
--- test/testMediator.dart
+++ test/testMediator.dart
@@ -6,7 +6,11 @@ void main () {
     var core = new Core();
     var received = null;
     
-    var subscription = core.mediator["channel"]["topic"].stream.listen((data){
+    core.mediator["channel"]["topic"].listen((data){
+      expect(data, equals("blabla"));
+    });
+    
+    core.mediator["channel"]["topic"].listen((data){
       expect(data, equals("blabla"));
     });
     
